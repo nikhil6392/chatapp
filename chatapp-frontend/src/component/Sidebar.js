cfab32a66d79813508889eced72567e9341737b3
nikhil6392 <nikhilpathak210431@gmail.com>
2024-12-30 19:38:09 +0530

    component for search bar

--- chatapp-frontend/src/component/Sidebar.js
+++ chatapp-frontend/src/component/Sidebar.js
@@ -5,12 +5,14 @@ import ChatIcon from '@mui/icons-material/Chat';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import profile from './assets/profile.jpg'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
         <div className='sidebar_header'>
-            <Avatar />
+            <Avatar src ={profile} />
             <div className='sidebar_headerRight'>
                 <IconButton>
                     <DonutLargeIcon />
@@ -23,7 +25,12 @@ const Sidebar = () => {
                 </IconButton>
             </div>
         </div>
-        <div className='sidebar_search'></div>
+        <div className='sidebar_search'>
+            <div className='sidebar_searchContainer'>
+                <SearchOutlinedIcon />
+                <input placeholder='Search bar'  type= "text" />
+            </div>
+        </div>
         <div className='sidebar_chats'></div>
     </div>
   )
