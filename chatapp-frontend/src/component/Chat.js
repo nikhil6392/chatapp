84a99ca3c5c7e387d21a8f753af08b164792df82
nikhil6392 <nikhilpathak210431@gmail.com>
2024-12-31 22:59:45 +0530

    build chatting component

--- chatapp-frontend/src/component/Chat.js
+++ chatapp-frontend/src/component/Chat.js
@@ -1,4 +1,4 @@
-import React , {useEffect, useState}from 'react'
+import React, { useEffect, useState } from 'react'
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
@@ -11,34 +11,56 @@ import './Chat.css'
 const Chat = () => {
     const [seed, setSeed] = useState("")
     useEffect(() => {
-        setSeed(Math.floor(Math.random()*5000))
-    },[])
-  return (
-    <div className='chat'>
-        <div className='chat_header'>
-            <Avatar src = {`https://api.dicebear.com/9.x/notionists/svg?${seed}`} />
-            <div className='chat_headerInfo'>
-                <h3>Room name</h3>
-                <p>last seen at ...</p>
+        setSeed(Math.floor(Math.random() * 5000))
+    }, [])
+    return (
+        <div className='chat'>
+            <div className='chat_header'>
+                <Avatar src={`https://api.dicebear.com/9.x/notionists/svg?${seed}`} />
+                <div className='chat_headerInfo'>
+                    <h3>Room name</h3>
+                    <p>last seen at ...</p>
+                </div>
+                <div className='chat_headerRight'>
+                    <IconButton>
+                        <SearchOutlinedIcon />
+                    </IconButton>
+                    <IconButton>
+                        <AttachFileIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
             </div>
-            <div className='chat_headerRight'>
-                <IconButton>
-                    <SearchOutlinedIcon />
-                </IconButton>
-                <IconButton>
-                    <AttachFileIcon />
-                </IconButton>
-                <IconButton>
-                    <MoreVertIcon />
-                </IconButton>
-            </div>
-        </div>
 
-        <div className='chat_body'></div>
-        <div className='chat_footer'></div>
-      
-    </div>
-  )
+            <div className='chat_body'>
+                <p className='chat_message'>
+                    <span className='chat_name'>Nikhil</span>
+                    this is a message
+                    <span className='chat_timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className='chat_message chat_receiver'>
+                    <span className='chat_name'>Parag</span>
+                    this is a message back
+                    <span className='chat_timestamp'>
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+                <p className="chat_message">
+                    <span className="chat_name">Nikhil</span>
+                    This is a message again again
+                    <span className="chat__timestamp">
+                        {new Date().toUTCString()}
+                    </span>
+                </p>
+            </div>
+            <div className='chat_footer'></div>
+
+        </div>
+    )
 }
 
 export default Chat
