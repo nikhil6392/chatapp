280d4bbe498e061b411968debed7a61dc987bef3
nikhil6392 <nikhilpathak210431@gmail.com>
2025-01-03 23:19:50 +0530

    msg model

---
+++ chatapp-backend/model/dbMessages.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const msgSchema = mongoose.Schema({
+    message: String,
+    name: String,
+    timestamp: String,
+    received: Boolean
+})
+
+export default mongoose.model('msgschemas', msgSchema)
