b4202af7e55b115233c67973055cd8d9d5faf3c3
nikhil6392 <nikhilpathak210431@gmail.com>
2025-01-06 03:47:19 +0530

    mapping of messages

--- chatapp-frontend/src/component/Chat.js
+++ chatapp-frontend/src/component/Chat.js
@@ -10,7 +10,7 @@ import MicIcon from '@mui/icons-material/Mic';
 
 
 
-const Chat = () => {
+const Chat = ({ messages}) => {
     const [seed, setSeed] = useState("")
     useEffect(() => {
         setSeed(Math.floor(Math.random() * 5000))
@@ -40,28 +40,16 @@ const Chat = () => {
             </div>
 
             <div className='chat_body'>
-                <p className='chat_message'>
-                    <span className='chat_name'>Nikhil</span>
-                    this is a message
-                    <span className='chat_timestamp'>
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-                <p className='chat_message chat_receiver'>
-                    <span className='chat_name'>Ritik</span>
-                    this is a message back
-                    <span className='chat_timestamp'>
-                        {new Date().toUTCString()}
-                    </span>
-                </p>
-                <p className="chat_message">
-                    <span className="chat_name">Nikhil</span>
-                    This is a message again again
+            {messages.map(message => (
+                <p className={`chat__message ${message.received && 'chat__receiver'}`}>
+                    <span className="chat__name">{message.name}</span>
+                            {message.message}
                     <span className="chat__timestamp">
-                        {new Date().toUTCString()}
+                        {message.timestamp}
                     </span>
                 </p>
-            </div>
+         ))}
+ </div>
             <div className='chat_footer'>
                 <InsertEmoticonIcon />
                 <form >
