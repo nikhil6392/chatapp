abf4417bb2f26baf6bff266dcb0331ebe9e99ad0
nikhil6392 <nikhilpathak210431@gmail.com>
2025-01-07 23:11:27 +0530

    connected backend with frontend

--- chatapp-frontend/src/App.js
+++ chatapp-frontend/src/App.js
@@ -1,13 +1,29 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Sidebar from './component/Sidebar';
 import Chat from './component/Chat';
+import axios  from 'axios';
+
 
 function App() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    axios.get("/messages/sync")
+      .then(res => {
+        setMessages(res.data); // Updating state with fetched data
+      })
+      .catch(err => {
+        console.error("Error fetching messages:", err);  // Handling errors
+      });
+  }); 
+
+
   return (
     <div className="App">
       <div className = "app_body">
             <Sidebar />
-            <Chat />
+            <Chat messages={messages}/>
       </div>
     </div>
   );
